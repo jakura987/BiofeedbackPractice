ef513395a6f32c736a063f8f65d40f7551a80d73
yuhang.ding <AAbb2754496408>
2026-06-01 13:38:48 +0800

    first commit

---
+++ app/src/main/java/com/intellizondy/biofeedback/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.intellizondy.biofeedback
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+//import com.intellizon.biofeedbacktest.BuildConfig
+
+import com.intellizondy.biofeedback.R
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+//        if (BuildConfig.DEBUG) {
+//            Timber.plant(Timber.DebugTree())
+//        }
+//        Timber.d("version name: ${BuildConfig.VERSION_NAME}")
+
+        findViewById<Button>(R.id.blueToothConnection).setOnClickListener {
+            Toast.makeText(this@MainActivity, "blueToothConnect", Toast.LENGTH_SHORT).show()
+        }
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+    }
+}
